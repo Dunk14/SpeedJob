b281534c3aefd9f5f67c7f37e2938c1179c1d372
Nicolas <nicolas.lepiez@imie-rennes.fr>
2017-03-07 10:35:13 +0100

    Merging conflict

--- front/src/app/home/home.component.ts
+++ front/src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, Injectable, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { AccountService } from "../services/account.service";
 
 @Component({
     selector: 'home',
@@ -19,11 +20,11 @@ export class HomeComponent implements OnInit {
          * Check that there's a user connected
          */
         this.user = JSON.parse(localStorage.getItem('user'));
-        if (!this.user) {
+        //if (!this.user) {
 
-            this.router.navigate(['/signIn']);
+        this.router.navigate(['/signIn']);
 
-        }
+        //}
     }
 
 }
