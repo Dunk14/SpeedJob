a0d4abcdd0759cc2d2013afdfdf6a77833df5d3b
Dunk14 <fosseykilyan@gmail.com>
2017-06-27 10:54:59 +0200

    Vuetify.js added

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,29 +1,9 @@
 <template>
-  <div id="app">
     <router-view></router-view>
-  </div>
 </template>
 
 <script>
-  import Vue from 'vue'
-  import 'bootstrap/dist/css/bootstrap.css'
-  import 'font-awesome/css/font-awesome.min.css'
-  import 'animate.css/animate.min.css'
-  import VueMdl from 'vue-mdl'
-  Vue.use(VueMdl)
-
-  export default {
-    name: 'app'
-  }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ front/src/components/views/sign-in.vue
@@ -0,0 +1,90 @@
+<style lang="scss">
+    body {
+        background: #f5f6f6;
+        background: -moz-linear-gradient(top, #f5f6f6 0%, #dbdce2 21%, #b8bac6 49%, #dddfe3 80%, #f5f6f6 100%);
+        background: -webkit-linear-gradient(top, #f5f6f6 0%,#dbdce2 21%,#b8bac6 49%,#dddfe3 80%,#f5f6f6 100%);
+        background: linear-gradient(to bottom, #f5f6f6 0%,#dbdce2 21%,#b8bac6 49%,#dddfe3 80%,#f5f6f6 100%);
+        filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#f5f6f6', endColorstr='#f5f6f6',GradientType=0 );
+    }
+
+    .form__connexion, .form__registration {
+        border-radius: 5px;
+        transition: all .3s linear;
+        overflow: hidden;
+    }
+
+    .form__connexion:hover, .form__registration:hover {
+        background-color: rgba(220,220,220,0.6);
+        padding: 10px;
+    }
+</style>
+
+<template>
+    <div class="container">
+        <div class="row">
+
+            <div class="col-sx-12">
+                <h1 class="logo text-align">SpeedJob</h1>
+
+                <p>Plateforme d'embauche entre les entreprises et l'école du numérique IMIE</p>
+            </div>
+
+            <div class="col-xs-12 col-sm-offset-2 col-sm-4">
+                <div class="form__connexion">
+                    <h4>Déjà membre ?</h4>
+
+                    <form>
+                        <v-text-field class="col-xs-12"
+                                      type="text"
+                                      label="Votre identifiant..."
+                                      v-model="login"
+                                      name="login"
+                                      @keyup.enter.native="logIn()"></v-text-field>
+
+                        <v-text-field class="col-xs-12"
+                                      type="password"
+                                      label="Mot de passe"
+                                      v-model="password"
+                                      name="password"
+                                      @keyup.enter.native="logIn()"></v-text-field>
+
+                        <v-btn @click.native="logIn()" @keyup.enter.native="logIn()" block>
+                            Se connecter
+                        </v-btn>
+                    </form>
+                </div>
+            </div>
+
+            <div class="col-xs-12 col-sm-4">
+                <div class="form__registration">
+                    <h5>Vous êtes chef d'entreprise et cherchez rapidement un étudiant conforme à vos attentes ?</h5>
+
+                    <h4>N'hésitez pas un instant de plus et inscrivez-vous ici <v-icon>arrow_downward</v-icon></h4>
+
+                    <v-btn @click.native="logIn()" block>
+                        S'inscrire
+                    </v-btn>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</template>
+
+<script>
+    import Vue from 'vue'
+
+    export default {
+        data () {
+            return {
+                login: "",
+                password: ""
+            }
+        },
+        methods: {
+            logIn () {
+                console.log(this.login)
+            }
+        }
+    }
+</script>
